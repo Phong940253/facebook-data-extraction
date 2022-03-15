ed31652cd989b3410995d6a6f5b3deb3ad772654
Phong <phong940253@gmail.com>
2022-03-16 00:02:59 +0700

    format file & fix crash null comment

--- 2 - Automation tools with IP hiding techniques/tool_label.py
+++ 2 - Automation tools with IP hiding techniques/tool_label.py
@@ -2,7 +2,9 @@ import json
 import os
 import pandas as pd
 
-#               Lấy các file json từ folder rawData
+# Lấy các file json từ folder rawData
+
+
 def getFile():
     listFile = []
     for root, directories, files in os.walk('rawData', topdown=False):
@@ -10,36 +12,46 @@ def getFile():
             listFile.append(os.path.join(root, name))
     return listFile
 
-#                 Dán nhãn
-def assignLabel(Content,Pre_Cmt,Cmt):
-    print("="*10+"POSTER"+"="*10+"\n")
+# Dán nhãn
+
+
+def assignLabel(Content, Pre_Cmt, Cmt):
+    print("=" * 10 + "POSTER" + "=" * 10 + "\n")
     print(Content)
     print("\n")
     if Pre_Cmt is not None:
-        for i,cmt in enumerate(Pre_Cmt):
-            print("="*5+f"Pre-Comment #{i}"+"="*5+"\n")
+        for i, cmt in enumerate(Pre_Cmt):
+            print("=" * 5 + f"Pre-Comment #{i}" + "=" * 5 + "\n")
             print(cmt)
-    print("="*10+"Comment"+"="*10+"\n")
+    print("=" * 10 + "Comment" + "=" * 10 + "\n")
     print(Cmt)
-    print("="*10+"\n")
+    print("=" * 10 + "\n")
     print("Comment này phù hợp hay không phù hợp ?")
-    cmtLabel = input("1=Phù hợp,0 = Không phù hợp: ")
+    cmtLabel = input("1 = Phù hợp, 0 = Không phù hợp: ")
     os.system('cls')
-    return [Content,Pre_Cmt,Cmt,cmtLabel]
+    return [Content, Pre_Cmt, Cmt, cmtLabel]
 
-#                     cleanData
+# cleanData
+
+
 def cleanData(filePath):
     Result = []
     with open(filePath, encoding="utf8") as f:
         data = json.loads(f.read())
         for cmt in data['Comment']:
-            Result.append(assignLabel(data['Content'], None, cmt['Cmt']))
-            if cmt['Number reply'] != 0:
-                pre = [cmt['Cmt']]
-                for i,rep in enumerate(cmt['Reply cmt']):
-                    pre += [parent["Cmt"] for parent in cmt['Reply cmt'][:i]]
-                    Result.append(assignLabel(data['Content'], pre, rep["Cmt"]))
-            print("Xong")
+            if cmt is not None:
+                Result.append(assignLabel(data['Content'], None, cmt['Cmt']))
+                if cmt['Number reply'] != 0:
+                    pre = [cmt['Cmt']]
+                    for i, rep in enumerate(cmt['Reply cmt']):
+                        pre += [parent["Cmt"]
+                                for parent in cmt['Reply cmt'][:i]]
+                        Result.append(
+                            assignLabel(
+                                data['Content'],
+                                pre,
+                                rep["Cmt"]))
+                print("Xong")
     return Result
 
 
@@ -47,8 +59,14 @@ def cleanData(filePath):
 # print(cleanData(filePath))
 listFile = getFile()
 for filePath in listFile:
-    df = pd.DataFrame(cleanData(filePath), columns=['Poster', 'Pre-Comment', 'Comment', 'Label'])
+    df = pd.DataFrame(
+        cleanData(filePath),
+        columns=[
+            'Poster',
+            'Pre-Comment',
+            'Comment',
+            'Label'])
     print(df)
     fileName = filePath.split("/")[-1].split(".")[0]
     # Lưu trong folder data
-    df.to_csv(f"./data/{fileName}.csv", encoding='utf-8',index=False)
+    df.to_csv(f"./data/{fileName}.csv", encoding='utf-8', index=False)
