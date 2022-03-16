97af02a0ce3e67067a76988586d952171affc421
Phong <phong940253@gmail.com>
2022-03-16 12:03:35 +0700

    fix location save file

--- 2 - Automation tools with IP hiding techniques/tool_label.py
+++ 2 - Automation tools with IP hiding techniques/tool_label.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import os
 import pandas as pd
@@ -5,27 +6,47 @@ import pandas as pd
 # Lấy các file json từ folder rawData
 
 
-def getFile():
-    listFile = []
-    for root, directories, files in os.walk('rawData', topdown=False):
-        for name in files:
-            listFile.append(os.path.join(root, name))
-    return listFile
+# def getFile():
+#     listFile = []
+#     for root, directories, files in os.walk('rawData', topdown=False):
+#         for name in files:
+#             listFile.append(os.path.join(root, name))
+#     return listFile
 
 # Dán nhãn
 
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+BGGREEN = '\x1b[6;30;42m'
+
+
 def assignLabel(Content, Pre_Cmt, Cmt):
-    print("=" * 10 + "POSTER" + "=" * 10 + "\n")
+    print(WARNING + "=" * 10 + "POSTER" + "=" * 10 + ENDC + "\n")
     print(Content)
     print("\n")
     if Pre_Cmt is not None:
         for i, cmt in enumerate(Pre_Cmt):
-            print("=" * 5 + f"Pre-Comment #{i}" + "=" * 5 + "\n")
+            print(
+                WARNING +
+                "=" *
+                5 +
+                f"Pre-Comment #{i}" +
+                "=" *
+                5 +
+                ENDC +
+                "\n")
             print(cmt)
-    print("=" * 10 + "Comment" + "=" * 10 + "\n")
+    print(WARNING + "=" * 10 + "Comment" + "=" * 10 + ENDC + "\n")
     print(Cmt)
-    print("=" * 10 + "\n")
+    print(WARNING + "=" * 10 + ENDC + "\n")
     print("Comment này phù hợp hay không phù hợp ?")
     cmtLabel = input("1 = Phù hợp, 0 = Không phù hợp: ")
     os.system('cls')
@@ -40,6 +61,7 @@ def cleanData(filePath):
         data = json.loads(f.read())
         for cmt in data['Comment']:
             if cmt is not None:
+                os.system('cls')
                 Result.append(assignLabel(data['Content'], None, cmt['Cmt']))
                 if cmt['Number reply'] != 0:
                     pre = [cmt['Cmt']]
@@ -51,13 +73,16 @@ def cleanData(filePath):
                                 data['Content'],
                                 pre,
                                 rep["Cmt"]))
-                print("Xong")
+                print(BGGREEN + "Xong" + ENDC)
     return Result
 
 
 # filePath = 'rawData/groups/364997627165697/1786546065010839.json'
 # print(cleanData(filePath))
-listFile = getFile()
+groupPost = glob.glob("rawData/*/*/*.json")
+pagePost = glob.glob("rawData/*/*.json")
+listFile = groupPost + pagePost
+
 for filePath in listFile:
     df = pd.DataFrame(
         cleanData(filePath),
@@ -67,6 +92,9 @@ for filePath in listFile:
             'Comment',
             'Label'])
     print(df)
-    fileName = filePath.split("/")[-1].split(".")[0]
+    fileName = filePath.split("\\")[-2]
+    # print(fileName)
     # Lưu trong folder data
+    # if not os.path.exists(f'data/{page}'):
+    #     os.makedirs(f'data/{page}')
     df.to_csv(f"./data/{fileName}.csv", encoding='utf-8', index=False)
